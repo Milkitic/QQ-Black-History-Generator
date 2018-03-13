97f300620c6e67aa4cc3ba6a186939e033339292
杨帆 <milkitic@126.com>
2018-03-13 20:30:58 +0800

    Initial commit

---
+++ Generator/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Generator.Models;
+using Generator.Function;
+
+namespace Generator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetImage()
+        {
+
+            PicGenerator gn = new PicGenerator();
+
+            return File(gn.GetImage("1234", "4231").ToArray(), "image/jpeg");
+
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Generator/Function/PicGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DrawingCore;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Generator.Function
+{
+    public class PicGenerator
+    {
+        public MemoryStream GetImage(string id, string message, Image avatar = null)
+        {
+            Bitmap bmp = new Bitmap(100, 35);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            g.FillRectangle(Brushes.Red, 2, 2, 125, 31);
+            g.DrawString("test", new Font("黑体", 15f), Brushes.Yellow, new PointF(5f, 5f));
+            MemoryStream ms = new MemoryStream();
+            bmp.Save(ms, System.DrawingCore.Imaging.ImageFormat.Png);
+            g.Dispose();
+            bmp.Dispose();
+            return ms;
+        }
+    }
+}
