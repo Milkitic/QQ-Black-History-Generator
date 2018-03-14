ecee463a1fd8f810d540b75f72db2b9c09d701b3
杨帆 <milkitic@126.com>
2018-03-14 19:19:27 +0800

    updated to full asp.net

--- Generator/Controllers/HomeController.cs
+++ Generator/Controllers/HomeController.cs
@@ -1,40 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Generator.Models;
-using Generator.Function;
+using System.Web;
+using System.Web.Mvc;
 
-namespace Generator.Controllers
+namespace Generator2.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult GetImage()
-        {
-
-            PicGenerator gn = new PicGenerator();
-
-            return File(gn.GetImage("1234", "4231").ToArray(), "image/jpeg");
-
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
